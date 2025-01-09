78af4c49b2757697607357008e75d0b3e1e245ee
كريم عبدالمنعم <145597061+Kvriem@users.noreply.github.com>
2025-01-09 08:48:56 +0200

    [1174] [Time Beats: 42.63%] - LeetPush

---
+++ 1174-Immediate-Food-Delivery-II.sql
@@ -0,0 +1,7 @@
+
+select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+from delivery d
+where (customer_id,order_date) in (
+    select customer_id ,min(order_date) as first_order 
+    from delivery
+    group by customer_id )
