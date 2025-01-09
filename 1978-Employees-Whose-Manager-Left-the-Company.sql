8dc7d604e9a393a890db3529c1127a9affec1ab6
كريم عبدالمنعم <145597061+Kvriem@users.noreply.github.com>
2025-01-09 08:51:24 +0200

    [1978] [Time Beats: 44.97%] - LeetPush

---
+++ 1978-Employees-Whose-Manager-Left-the-Company.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select employee_id 
+from Employees  
+WHERE salary < 30000 
+AND manager_id NOT IN (SELECT employee_id FROM Employees)
+order by employee_id
